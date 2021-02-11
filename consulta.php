fb5ea1400c9600d3eb1f1d5a9e9934d179510696
JoseGarPe <jhosuegarciastarkand@gmail.com>
2021-02-11 16:24:38 -0600

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -9,9 +9,11 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css" rel="stylesheet" />
 </head>
 <body>
-                <div class="table-responsive">
+<div class="container">
+  <div class="table-responsive">
 					<table id="informacionCotizacion" class="display table" style="width:100%">
 				        <thead>
 				            <tr>
@@ -39,10 +41,16 @@
 				    </table>
 				</div>
 
- 
+</div>
+              
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.jss"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
 <script>
 	$(document).ready(function(){
 		infoMovimientos();
@@ -78,7 +86,13 @@ function infoMovimientos(){
 					}
 			
 				informacion.innerHTML=imprimir;
-				$('#example22s').DataTable();
+				$('#example22s').DataTable({
+                                
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'copy', 'csv', 'excel', 'pdf', 'print'
+                    ]
+                });
 				//  informacionInversion();
 		
 	    }
